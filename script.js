8ca6189493e80d545b3a7a8d68106bad0228df6c
P-Worthington <pworthington17@gmail.com>
2025-06-10 16:54:56 +0100

    complete

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const subtext = document.querySelector(".subtext");
 
   setTimeout(() => {
-    subtext.textContent = "Our full site is coming soon. Stay tuned.";
     subtext.style.opacity = "1";
   }, 1500);
 });
